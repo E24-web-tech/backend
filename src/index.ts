ea519acc7497accd3818fcfe595d268da0a496a3
BitOfAByte <hopetobylol@gmail.com>
2024-11-11 10:44:21 +0100

    Init

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import express from 'express';
+(() => {
+	const app = express();
+
+	app.get('/', (_req, res) => {
+		res.send('Hello World!');
+	});
+
+	app.listen(process.env.PORT, () => {
+		console.log(
+			`Server is running on http://localhost:${process.env.PORT}`
+		);
+	});
+})();
